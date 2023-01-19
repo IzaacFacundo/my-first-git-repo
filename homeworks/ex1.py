7372b4fae8e197c56d52d608832bca685cc41ec3
Izaac Facundo <izaacfacundo@utexas.edu>
2023-01-19 11:39:45 -0600

    Created homeworks folder

---
+++ homeworks/ex1.py
@@ -0,0 +1,20 @@
+# comment
+words = []
+
+with open('/usr/share/dict/words', 'r') as f:
+    words = f.read().splitlines()
+topfive = [words[0],words[1],words[2],words[3],words[4]]
+smallest = 0
+
+for word in words:
+	for x in range(0,5):
+		if len(topfive[x]) < len(topfive[smallest]):
+			smallest = x
+
+	if len(word) > len(topfive[smallest]):
+		topfive[smallest] = word
+
+for x in range(5):
+	print(topfive[x])
+
+	
